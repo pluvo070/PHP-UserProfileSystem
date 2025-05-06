fca8070f9129db2fa8d4e3dfbf92ac95b0d01b48
pluvo070 <30020422+pluvo070@users.noreply.github.com>
2025-05-06 21:36:07 +0100

    使用composer

--- user_system/config.php
+++ user_system/config.php
@@ -1,10 +1,10 @@
 <?php
-// 用于连接数据库
-
+// 用于连接数据库: 面向对象风格, 使用 mysqli 类
 $servername = "localhost";
-$username = "admin";   // 默认用户名
+// $username = "admin";     // PHPStudy的MySQL服务默认用户名
+$username = "root";         // 本地MySQL的默认用户名
 $password = "123456";       // 默认密码
-$dbname = "user_system";  // 数据库名
+$dbname = "user_system";    // 数据库名
 
 // 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
